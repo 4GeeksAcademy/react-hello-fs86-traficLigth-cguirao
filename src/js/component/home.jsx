aff2e3d56c801aae6c9b1d82625ffd4fb286fd86
Cristian-svg598 <criguies@gmail.com>
2024-12-10 10:41:35 +0000

    Semaforo acabado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "../../styles/home.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const colors = ["Rojo", "Amarillo", "Verde"];
+  const [currentColor, setCurrentColor] = useState(colors[0]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentColor((prevColor) => {
+        const nextIndex = (colors.indexOf(prevColor) + 1) % colors.length;
+        return colors[nextIndex];
+      });
+    }, 2000);
+
+    // Limpiar el intervalo al desmontar el componente
+    return () => clearInterval(interval);
+  }, []); // [] asegura que el efecto se ejecute solo una vez al montar
+
+  return (
+    <div className="semaforo">
+      <div className={`luz ${currentColor === "Rojo" ? "rojo" : ""}`}></div>
+      <div className={`luz ${currentColor === "Amarillo" ? "amarillo" : ""}`}></div>
+      <div className={`luz ${currentColor === "Verde" ? "verde" : ""}`}></div>
+    </div>
+  );
 };
 
 export default Home;
